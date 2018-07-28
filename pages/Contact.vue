b97ca7bf9cd433e39abd993f6b0c4626dcc73b52
Kenil <kenilsha93@gmail.com>
2018-07-28 17:14:16 +0530

    Initial Commit

---
+++ pages/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+ <div class='container'>
+   <div class = 'text'>
+      <nuxt-link to="/" class="home-button">Home
+     </nuxt-link>
+     <h1>Contact</h1>
+
+   </div>
+
+<div class="left">
+  </div>
+    
+ </div>  
+  
+ 
+ </template>
+
+<script>
+export default {
+  data : function(){
+    return {}
+  },
+  methods :{
+    Details1 : function(){
+      this.$router.push("details1");
+    }
+  } 
+}
+</script>
+
+<style >
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+ <div class='container'>
+   <div class = 'text'>
+     <h1>Hi, I am Kenil Shah</h1>
+     <p class='tag'> Web designer & Development</p>
+   </div>
+
+   <div class='left'>     
+     <button v-on:click="toggleSidebar" class="menu-button">
+       Menu
+     </button>
+  <span class="nav"></span>
+  <ul class="side-nav" :class="isOpen ? 'open' : ''">
+    <li><nuxt-link to="/">Menu</nuxt-link></li>
+    <li><nuxt-link to ="/Resume">Resume</nuxt-link></li>
+    <li><nuxt-link to="/Contact"> Contact</nuxt-link></li>
+  </ul>
+  
+      
+   </div>  
+ </div>  
+  
+ 
+ </template>
+
+<script>
+export default {
+  data(){
+    return {
+      isOpen: false
+    }
+  },
+  methods :{
+    toggleSidebar() {
+      debugger
+      this.isOpen = !this.isOpen  
+    }
+  } 
+}
+</script>
+
